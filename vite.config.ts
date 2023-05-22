a452ac0dfcf5c8af6126684b38308ab58be44065
Chris Snowden <chrissnowden35@gmail.com>
2023-05-22 17:34:52 +0100

    Update base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://cpsnowden.github.io/swimlane-ui/",
+  base: "https://swimminglane.co.uk",
   plugins: [react()],
   resolve: {
     alias: {
